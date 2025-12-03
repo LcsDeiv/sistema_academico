0142317e30569835caf7b85fcd215ceb4a947062
LcsDeiv <lucasdeivson2012@gmail.com>
2025-12-03 18:53:57 -0300

    Correção

--- app/services/professor_service.py
+++ app/services/professor_service.py
@@ -1,22 +1,17 @@
 from sqlalchemy.orm import Session
-from app.models.aluno_model import ProfessorModel
-from fastapi import HTTPException, status
-from datetime import date
+from app.models.professor_model import Professor
 
-class ProfessorService:
+def criar_professor(db: Session, nome: str, email: str):
+    professor_existente = db.query(Professor).filter(Professor.email == email).first()
+    if professor_existente:
+        raise ValueError("Email já cadastrado!")
 
-    @staticmethod
-    def criar_professor(db: Session, nome: str, departamento: str, email: str) -> ProfessorModel:
-        existing_professor = db.query(ProfessorModel).filter(ProfessorModel.email == email).first()
-        if existing_professor:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Email Já Cadastrado."
-            )
-        novo_professor = ProfessorModel(nome=nome, departamento=departamento, email=email)
-        db.add(novo_professor)
-        db.commit()
-        db.refresh(novo_professor)
-        return novo_professor
-    
-    
+    novo_professor = Professor(nome=nome, email=email)
+    db.add(novo_professor)
+    db.commit()
+    db.refresh(novo_professor)
+    return novo_professor
+
+
+def listar_professores(db: Session):
+    return db.query(Professor).all()
